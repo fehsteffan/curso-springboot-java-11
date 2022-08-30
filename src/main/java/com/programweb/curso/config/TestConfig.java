28902e7080589e236bad9f23e237b2751cea4e24
Felipe Steffan <felip.developer@hotmail.com>
2022-08-29 21:23:38 -0300

    Product

--- src/main/java/com/programweb/curso/config/TestConfig.java
+++ src/main/java/com/programweb/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.programweb.curso.entities.Category;
 import com.programweb.curso.entities.Order;
+import com.programweb.curso.entities.Product;
 import com.programweb.curso.entities.Usuary;
 import com.programweb.curso.entities.enums.OrderStatus;
 import com.programweb.curso.repositories.CategoryRepository;
 import com.programweb.curso.repositories.OrderRepository;
+import com.programweb.curso.repositories.ProductRepository;
 import com.programweb.curso.repositories.UsuaryRepository;
 
 @Configuration
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -38,7 +43,15 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null," Books ");
 		Category cat3 = new Category(null, " computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+					
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 				
 		
 		Usuary u1 = new Usuary(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
@@ -51,8 +64,6 @@ public class TestConfig implements CommandLineRunner {
 				
 		usuaryRepository.saveAll(Arrays.asList(u1, u2));	
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-
 		
 	}
 
---
+++ src/main/java/com/programweb/curso/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.programweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.programweb.curso.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository <Product, Long> {
+	
+	
+
+}
